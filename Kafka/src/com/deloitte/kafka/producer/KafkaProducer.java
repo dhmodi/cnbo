d8f0da942d67f5923a795d9e97e327dfdc8b14ab
Dhaval Modi <dhavalmodi24@gmail.com>
2014-07-17 03:29:46 -0700

    Kafka Producer

---
+++ Kafka/src/com/deloitte/kafka/producer/KafkaProducer.java
@@ -0,0 +1,74 @@
+package com.deloitte.kafka.producer;
+
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import java.util.*;
+import java.io.*;
+
+import com.deloitte.kafka.util.ProducerConstant;
+
+
+public class KafkaProducer  {
+
+	public static void main(String[] args) {
+		String propertiesPath = args[0];
+
+		Process p;
+		Properties props = new Properties();
+		String line = "";
+		// Random rnd = new Random();
+		String msg = "";
+		String num = "1";
+
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream(propertiesPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		String Command = "tail -f "
+				+ properties
+						.getProperty(ProducerConstant.INPUT_FILE_NAME);
+		String Broker_List = properties
+				.getProperty(ProducerConstant.BROKER_LIST);
+		String Serializer_type = properties
+				.getProperty(ProducerConstant.SERIALIZER_TYPE);
+		String Num_Of_Ack = properties
+				.getProperty(ProducerConstant.NUM_OF_ACK);
+		String Topic_To_Write = properties
+				.getProperty(ProducerConstant.TOPIC_TO_WRITE);
+		props.put("metadata.broker.list", Broker_List);
+		props.put("serializer.class", Serializer_type);
+		// props.put("partitioner.class", "example.producer.SimplePartitioner");
+		props.put("request.required.acks", Num_Of_Ack);
+
+		ProducerConfig config = new ProducerConfig(props);
+		Producer<String, String> producer = new Producer<String, String>(config);
+
+		try {
+			p = Runtime.getRuntime().exec(Command);
+			// p.waitFor();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+
+			while ((line = reader.readLine()) != null) {
+
+				msg = line + "\n";
+				KeyedMessage<String, String> data = new KeyedMessage<String, String>(
+						Topic_To_Write, num, msg);
+				producer.send(data);
+
+			}
+			producer.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+
---
+++ Kafka/src/com/deloitte/kafka/util/ProducerConstant.java
@@ -0,0 +1,9 @@
+package com.deloitte.kafka.util;
+
+public interface ProducerConstant {
+	public static final String INPUT_FILE_NAME = "producer.file.name";	
+	public static final String BROKER_LIST = "producer.broker.list";	
+	public static final String SERIALIZER_TYPE = "producer.serializer.type";
+	public static final String NUM_OF_ACK = "producer.ack.request";
+	public static final String TOPIC_TO_WRITE = "producer.topic.name";
+}
