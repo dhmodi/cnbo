8a1310eb9129d11cd92ee6f1efcce399fb5629e8
Dhaval Modi <dhavalmodi24@gmail.com>
2014-07-16 20:54:45 -0700

    HDFS change

--- Extractor/src/com/deloitte/extractor/Extractor.scala
+++ Extractor/src/com/deloitte/extractor/Extractor.scala
@@ -8,15 +8,20 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import scala.collection.Iterator
 import java.io._
+import java.util.ArrayList
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.SequenceFile
 import org.apache.hadoop.io.Text
 import org.apache.hadoop.io.IntWritable
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
+import scala.AnyVal
 
 object Extractor {
 
+  val arrString: Map[String, ListBuffer[Tuple4[String, String, BigInt, Int]]] = Map()
   val sc = new SparkContext(new SparkConf().setMaster("spark://localhost.localdomain:7077").setAppName("WeblogExtractor"))
 
   def main(args: Array[String]): Unit = {
@@ -25,12 +30,13 @@ object Extractor {
 
     val textFile = sc.textFile("input.txt").map(line => line.split(" ")).map(line => (line(0), (line(3), line(6)))).groupByKey
 
-    var timeOnPageSec: Long = 0
+    var timeOnPageSec: BigInt = 0
 
     var noOfPagesPerSession = 0
 
-    var totalTimeOnSite: Long = 0
+    var totalTimeOnSite: BigInt = 0
 
+    var arrRemFields: ListBuffer[Tuple4[String, String, BigInt, Int]] = ListBuffer()
     for ((ipAddress, seqData) <- textFile) {
 
       seqData.sortBy(seqData => seqData._1)
@@ -38,7 +44,7 @@ object Extractor {
       val iterator: Iterator[(String, String)] = seqData.iterator
 
       while (iterator.hasNext) {
-
+        var noOfVisits = 1;
         val varTuple = iterator.next
 
         var varDateTime = varTuple._1
@@ -94,10 +100,14 @@ object Extractor {
           }
 
         }
-        //writer.write(ipAddress + "," + varDate.toString + "," + pageURL + "," + duration.toString)
+        val writer = new Tuple4(varDate.toString, pageURL, timeOnPageSec, noOfVisits)
+        arrRemFields += writer
 
+        //writer.write(ipAddress + "," + varDate.toString + "," + pageURL + "," + duration.toString + "," + Pagevisits)
+        timeOnPageSec = 0
+        noOfVisits = 0
       }
-
+      arrString += ipAddress -> arrRemFields
     }
 
   }
